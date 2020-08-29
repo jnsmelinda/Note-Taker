e17cce3cd3662f5d77e93b7799049a26b5a14054
Melinda Ivanov <jns.melinda@gmail.com>
2020-08-28 20:17:26 -0700

    generating note id

--- dao.js
+++ dao.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const { v4: uuid } = require('uuid');
 
 function getNotes(processDataFunction) {
     fs.readFile("db/db.json", "utf8", (err, data) => {
@@ -9,6 +10,7 @@ function getNotes(processDataFunction) {
 
 function saveNote(note, callback) {
     getNotes(data => {
+       note.id = uuid();
        data.push(note);
        fs.writeFile("db/db.json", JSON.stringify(data), (err) => {
            if (err) throw err;
