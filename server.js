bf5479fda73ae9163095c80cc37c04ca1c76049d
Melinda Ivanov <jns.melinda@gmail.com>
2020-08-29 17:29:20 -0700

    preparing for heroku deploy

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const dao = require("./dao");
 const { request } = require("http");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
